521e91f3dfcbf493d13a08799aabd8e11616d190
KeidsID <keidscode@gmail.com>
2024-07-11 19:57:24 +0800

    feat(interfaces): add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import MainPage from "./interfaces/pages/main_page";
+import SignUpPage from "./interfaces/pages/auth/sign_up_page";
+import SignInPage from "./interfaces/pages/auth/sign_in_page";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainPage />,
+  },
+  {
+    path: "/sign-up",
+    element: <SignUpPage />,
+  },
+  {
+    path: "/sign-in",
+    element: <SignInPage />,
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <MainPage />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
